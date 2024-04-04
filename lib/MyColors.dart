badfa3781c5366d10764f11e116100a730d053fd
KiaGhorbani <kiarashghorbani@gmail.com>
2024-04-04 20:45:33 +0330

    Added FlutterGen & Localized

---
+++ lib/MyColors.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: file_names, constant_identifier_names, unnecessary_const
+import 'dart:ui';
+
+class SolidColors {
+  static const Color ColorTitle = Color.fromARGB(255, 40, 107, 184);
+  static const Color TextTitle = Color.fromARGB(255, 0, 0, 0);
+  static const Color ScaffoldBg = Color.fromARGB(255, 255, 255, 255);
+}
+
+class GradientColors {
+  static const List<Color> BottomNav = [
+    const Color.fromARGB(255, 25, 0, 94),
+    const Color.fromARGB(255, 68, 4, 87)
+  ];
+}
---
+++ lib/MyStrings.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: file_names, constant_identifier_names
+class Strings {
+  static const String HotBlogs = "مشاهده داغ ترین نوشته ها";
+  static const String Hotpodcasts = "مشاهده داغ ترین پادکست ها";
+  static const String RelatedArticles = "نوشته های مرتبط";
+}
